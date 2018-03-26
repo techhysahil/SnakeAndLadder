d2f4f6f383d1a0d0e14caf1d716de69ef19cd3fc
Sahil <sahilkkrazy@gmail.com>
2018-03-26 17:28:11 +0530

    added route redirect logic

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { push } from 'react-router-redux'
 import { connect } from 'react-redux';
-import { history } from "../store.js";
+import { history, store } from "../store.js";
 import * as Action from "../actions/index.js";
 import reducer from '../reducers/index'
 
@@ -13,17 +13,31 @@ const mapStateToProps = ( state, ownProps ) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    singlePlayer: () => dispatch( Action.singlePlayer() ),
+    singlePlayer: () => dispatch(Action.singlePlayer()),
     multiPlayer: () => dispatch( Action.multiPlayer() ),
 });
 
-const Start = props => (
-	<div className="choose-players">
-		<div className="title">Select Game Mode</div>
-		<div className="single opt" onClick={() => props.singlePlayer()}>Single Player</div>
-		<div className="multiple opt" onClick={() => props.multiPlayer()}>Multple Player</div>
-	</div>
-)
+class Start extends Component {
+	
+	selectGameMode(str){ 
+		if(str === "single"){
+			this.props.singlePlayer()
+		}else if(str === "multiple"){
+			this.props.multiPlayer()
+		}
+		store.dispatch(push('/detail'))
+	}
+
+	render(){
+		return (
+			<div className="choose-players">
+				<div className="title">Select Game Mode</div>
+				<div className="single opt" onClick={() => this.selectGameMode()}>Single Player</div>
+				<div className="multiple opt" onClick={() => this.selectGameMode()}>Multple Player</div>
+			</div>
+		)
+	}
+}
 
 
 const ConnectStart = connect(
